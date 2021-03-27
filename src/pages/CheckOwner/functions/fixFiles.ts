f73734ed6abe7414a930a1980e22c68f2405bbd3
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-03-27 20:59:37 -0300

    fix: 'while true' bug when newTerm contains oldTerm

--- src/pages/CheckOwner/functions/fixFiles.ts
+++ src/pages/CheckOwner/functions/fixFiles.ts
@@ -4,38 +4,30 @@ import { SwitchTermPair } from '../../Settings/entities/SwitchTermPair'
 
 import IFile from '../interfaces/IFile'
 
+const removeBlankPairs = (termPairs: SwitchTermPair[]) =>
+  termPairs.filter(({ newTerm, oldTerm }) => newTerm || oldTerm)
+
 const fixFiles = async (
   filesToFix: IFile[],
   switchTermPairs: SwitchTermPair[]
 ): Promise<void> => {
+  const promisedWriteFile = promisify(fs.writeFile)
+
   const promisedLoops = filesToFix.map(async (file: IFile) => {
-    switchTermPairs = switchTermPairs.filter(
-      ({ newTerm, oldTerm }) => newTerm || oldTerm
-    )
+    let fileContent = await file.text()
 
-    const fileContent = await file.text()
+    removeBlankPairs(switchTermPairs).forEach(async ({ oldTerm, newTerm }) => {
+      const oldTermRegex = new RegExp(oldTerm, 'gi')
 
-    switchTermPairs.forEach(async ({ oldTerm, newTerm }) => {
-      const regexToBeSubstituted = new RegExp(oldTerm, 'i')
-
-      let fixedFileContent = fileContent
-
-      while (fixedFileContent.match(regexToBeSubstituted)) {
-        fixedFileContent = fixedFileContent.replace(
-          regexToBeSubstituted,
-          newTerm
-        )
-      }
-
-      const promisedWriteFile = promisify(fs.writeFile)
-
-      try {
-        await promisedWriteFile(file.path, fixedFileContent)
-      } catch (error) {
-        alert('An error ocurred updating the file' + error.message)
-        console.error(error)
-      }
+      fileContent = fileContent.replace(oldTermRegex, newTerm)
     })
+
+    try {
+      await promisedWriteFile(file.path, fileContent)
+    } catch (error) {
+      alert('An error ocurred updating the file' + error.message)
+      console.error(error)
+    }
   })
 
   await Promise.all(promisedLoops)
